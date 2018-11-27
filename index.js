02827c0bc79355f5dee5aff4314ce76f3e3df8f4
Unknown <felix.sabatie@gmail.com>
2018-11-27 09:54:53 -0500

    Better separation of files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const router = require('./routes')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -7,27 +8,6 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-const redirectConsoleToFunctions = require('./console-redirector').redirectConsoleToFunctions
-const restoreConsoleFunctions = require('./console-redirector').restoreConsoleFunctions
-
-app.post('/', function (req, res) {
-  let logs = ''
-  const consoleLog = (message) => {
-    logs += message + '\n'
-  }
-
-  console.log('Executing...')
-  redirectConsoleToFunctions(consoleLog, consoleLog, consoleLog)
-  try {
-    let userFunction = Function(req.body.function)
-    userFunction()
-  } catch(err) {
-    console.error(err)
-  }
-  restoreConsoleFunctions()
-  console.log('Done')
-
-  res.send(logs)
-})
+app.use('/api', router)
 
 app.listen(3000)
---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const redirectConsoleToFunctions = require('../utils/console-redirector').redirectConsoleToFunctions
+const restoreConsoleFunctions = require('../utils/console-redirector').restoreConsoleFunctions
+
+router.post('/', function (req, res) {
+  let logs = ''
+  const consoleLog = (message) => {
+    logs += message + '\n'
+  }
+
+  console.log('Executing...')
+  redirectConsoleToFunctions(consoleLog, consoleLog, consoleLog)
+  try {
+    let userFunction = Function(req.body.function)
+    userFunction()
+  } catch(err) {
+    console.error(err)
+  }
+  restoreConsoleFunctions()
+  console.log('Done')
+
+  res.send(logs)
+})
+
+module.exports = router
