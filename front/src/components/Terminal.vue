ba85a38d7c53ef624db072410e1453853a3d2229
Unknown <felix.sabatie@gmail.com>
2018-11-27 17:43:55 -0500

    Better terminal styling

--- front/src/components/Terminal.vue
+++ front/src/components/Terminal.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="terminal">
-    <div class="title">Results</div>
-    <div class="logs">
-      <div class="log" v-for="log in logs" :class="{error: log.isError, warn: log.isWarning}">
-        {{log.message}}
+  <div class="terminal-wrapper">
+    <div class="terminal">
+      <div class="title">Results</div>
+      <div class="logs">
+        <div class="log" v-for="log in logs" :class="{error: log.isError, warn: log.isWarning}">
+          {{log.message}}
+        </div>
       </div>
     </div>
   </div>
@@ -18,25 +20,39 @@
 <style lang="scss">
   @import '../styles/colors';
 
-  .terminal {
-    color: $white-text;
+  .terminal-wrapper {
+    position: relative;
+    height: 100%;
 
-    .title {
-      padding: 10px;
-      color: $grey-text;
-      background-color: $darker-background;
-    }
+    .terminal {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
 
-    .logs {
-      padding: 10px;
-      font-family: Inconsolata, monospace;
+      color: $white-text;
+      overflow: auto;
 
-      .log {
-        &.error {
-          color: $error-text;
-        }
-        &.warn {
-          color: $warn-text;
+      .title {
+        padding: 10px;
+        color: $grey-text;
+        background-color: $darker-background;
+        position: sticky;
+        top: 0;
+      }
+
+      .logs {
+        padding: 10px;
+        font-family: Inconsolata, monospace;
+
+        .log {
+          &.error {
+            color: $error-text;
+          }
+          &.warn {
+            color: $warn-text;
+          }
         }
       }
     }
